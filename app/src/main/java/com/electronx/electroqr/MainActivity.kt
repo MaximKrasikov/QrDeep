baf62e47f28572eaf13ad8a404e377d4486c4bd7
ÐMaxim Krasikov <karasy77@yandex.ru>
2023-08-02 00:17:17 +0300

    почистил код

--- app/src/main/java/com/electronx/electroqr/MainActivity.kt
+++ app/src/main/java/com/electronx/electroqr/MainActivity.kt
@@ -1,51 +1,24 @@
 package com.electronx.electroqr
 
-import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.annotation.VisibleForTesting
-import androidx.appcompat.app.AlertDialog
 import com.electronx.electroqr.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.dynamiclinks.PendingDynamicLinkData
-import com.google.firebase.dynamiclinks.ktx.androidParameters
-import com.google.firebase.dynamiclinks.ktx.dynamicLink
 import com.google.firebase.dynamiclinks.ktx.dynamicLinks
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // [START_EXCLUDE]
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val linkSendTextView = binding.linkViewSend
         val linkReceiveTextView = binding.linkViewReceive
-
         // Validate that the developer has set the app code.
-        validateAppCode()
-
-        // Create a deep link and display it in the UI
-        val newDeepLink = buildDeepLink(Uri.parse(DEEP_LINK_URL), 0)
-        linkSendTextView.text = newDeepLink.toString()
-
-        // Share button click listener
-        binding.buttonShare.setOnClickListener { shareDeepLink(newDeepLink.toString()) }
-        // [END_EXCLUDE]
-
-        /*binding.buttonShareShortLink.setOnClickListener {
-            val shortLinkTextView = findViewById<TextView>(R.id.shortLinkViewSend)
-            val shortDynamicLink = shortLinkTextView.text
-            shareDeepLink(shortDynamicLink.toString())
-        }*/
-
-        /* binding.buttonGenerateShortLink.setOnClickListener {
-             val deepLink = Uri.parse(DEEP_LINK_URL)
-            // buildShortLinkFromParams(deepLink, 0)
-         }*/
+        //  validateAppCode()
 
         // [START get_deep_link]
         Firebase.dynamicLinks
@@ -82,63 +55,9 @@ class MainActivity : AppCompatActivity() {
     }
     // [END on_create]
 
-    /**
-     * Build a Firebase Dynamic Link.
-     * https://firebase.google.com/docs/dynamic-links/android/create#create-a-dynamic-link-from-parameters
-     *
-     * @param deepLink the deep link your app will open. This link must be a valid URL and use the
-     * HTTP or HTTPS scheme.
-     * @param minVersion the `versionCode` of the minimum version of your app that can open
-     * the deep link. If the installed app is an older version, the user is taken
-     * to the Play store to upgrade the app. Pass 0 if you do not
-     * require a minimum version.
-     * @return a [Uri] representing a properly formed deep link.
-     */
-    @VisibleForTesting
-    fun buildDeepLink(deepLink: Uri, minVersion: Int): Uri {
-        val uriPrefix = getString(R.string.dynamic_links_uri_prefix)
 
-        // Set dynamic link parameters:
-        //  * URI prefix (required)
-        //  * Android Parameters (required)
-        //  * Deep link
-        // [START build_dynamic_link]
-        // Build the dynamic link
-        val link = Firebase.dynamicLinks.dynamicLink {
-            domainUriPrefix = uriPrefix
-            androidParameters {
-                minimumVersion = minVersion
-            }
-            link = deepLink
-        }
-        // [END build_dynamic_link]
-
-        // Return the dynamic link as a URI
-        return link.uri
-    }
-    /* @VisibleForTesting
-     fun buildShortLinkFromParams(deepLink: Uri, minVersion: Int) {
-         val uriPrefix = getString(R.string.dynamic_links_uri_prefix)
-
-         // Set dynamic link parameters:
-         //  * URI prefix (required)
-         //  * Android Parameters (required)
-         //  * Deep link
-         Firebase.dynamicLinks.shortLinkAsync {
-             link = deepLink
-             domainUriPrefix = uriPrefix
-             androidParameters {
-                 minimumVersion = minVersion
-             }
-         }.addOnSuccessListener { (shortLink, flowchartLink) ->
-             val shortLinkTextView = findViewById<TextView>(R.id.shortLinkViewSend)
-             shortLinkTextView.text = shortLink.toString()
-         }.addOnFailureListener(this) { e ->
-             Log.e(TAG, e.toString())
-         }
-     }*/
-
-    private fun shareDeepLink(deepLink: String) {
+    //расшарить диплинк
+    /*private fun shareDeepLink(deepLink: String) {
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "text/plain"
         intent.putExtra(Intent.EXTRA_SUBJECT, "Firebase Deep Link")
@@ -146,21 +65,8 @@ class MainActivity : AppCompatActivity() {
 
         startActivity(intent)
     }
-
-    private fun validateAppCode() {
-        val uriPrefix = getString(R.string.dynamic_links_uri_prefix)
-        if (uriPrefix.contains("YOUR_APP")) {
-            AlertDialog.Builder(this)
-                .setTitle("Invalid Configuration")
-                .setMessage("Please set your Dynamic Links domain in app/build.gradle")
-                .setPositiveButton(android.R.string.ok, null)
-                .create().show()
-        }
-    }
-
+*/
     companion object {
-
         private const val TAG = "MainActivity"
-        private const val DEEP_LINK_URL = "https://avpower.ru"
     }
 }
